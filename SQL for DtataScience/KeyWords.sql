13102665f64b2d5d6be99a27e38fbdaf9203d1d5
sagsango <34793802+sagsango@users.noreply.github.com>
2019-10-27 00:09:15 +0530

    Create KeyWords.sql

---
+++ SQL for DtataScience/KeyWords.sql
@@ -0,0 +1,9 @@
+[select ,select distinct ] ___ as c1, ___ as c2, ___ as c3
+from ___ as t1
+join ___ as t2
+on t1.___ = t2.___
+where   t1.___ [ in , = , >= , <= , != , like ] [ and , or ][ between l and r ]
+having  [ aggrigration : sum ,avg , count ..]
+order by __ desc
+limit MAX_ROW;
+
