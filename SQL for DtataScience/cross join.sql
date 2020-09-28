4b874a2995e4563ebae6848a446be71f1b7c7625
Sagar Singh <34793802+sagsango@users.noreply.github.com>
2020-09-29 02:23:55 +0530

    Update cross join.sql

--- SQL for DtataScience/cross join.sql
+++ SQL for DtataScience/cross join.sql
@@ -1,19 +1,18 @@
 Q : Find all costumer having order >= average on of orders per costumer
-with t1 as (
+with 
+t1 as (
   	select count(*) as total_order
-  	from orders
-  ),
-  t2 as(
+  	from orders ),
+t2 as(
   	select count(*) as total_account
-    from accounts
-  ),
+    from accounts ),
 t3 as (  
-select  a.id, count(*) as cnt
-from accounts as a
-join orders as o
-on a.id = o.account_id
-group by a.id
-order by a.id )
+    select  a.id, count(*) as cnt
+    from accounts as a
+    join orders as o
+    on a.id = o.account_id
+    group by a.id
+    order by a.id )
 
 select t3.id , t3.cnt
 from t3 
@@ -22,21 +21,20 @@ cross join t1
 where cnt >= total_order / total_account;
 
 -- see this table for better understanding
-with t1 as (
+with
+t1 as (
   	select count(*) as total_order
-  	from orders
-  ),
-  t2 as(
+  	from orders ),
+t2 as(
   	select count(*) as total_account
-    from accounts
-  ),
+    from accounts ),
 t3 as (  
-select  a.id, count(*) as cnt
-from accounts as a
-join orders as o
-on a.id = o.account_id
-group by a.id
-order by a.id )
+    select  a.id, count(*) as cnt
+    from accounts as a
+    join orders as o
+    on a.id = o.account_id
+    group by a.id
+    order by a.id )
 
 select *
 from t3 
