7757271d6f676c8548287aef9c40fcd1e00666a5
Sagar Singh <sagar.it.iiests@gmail.com>
2023-10-21 12:41:57 -0500

    Create pattern.sql

---
+++ SQL for DtataScience/pattern.sql
@@ -0,0 +1,34 @@
+mysql> SELECT * FROM pet WHERE name LIKE '%fy';
++--------+--------+---------+------+------------+-------+
+| name   | owner  | species | sex  | birth      | death |
++--------+--------+---------+------+------------+-------+
+| Fluffy | Harold | cat     | f    | 1993-02-04 | NULL  |
+| Buffy  | Harold | dog     | f    | 1989-05-13 | NULL  |
++--------+--------+---------+------+------------+-------+
+To find names containing a w:
+
+
+mysql> SELECT * FROM pet WHERE name LIKE '%w%';
++----------+-------+---------+------+------------+------------+
+| name     | owner | species | sex  | birth      | death      |
++----------+-------+---------+------+------------+------------+
+| Claws    | Gwen  | cat     | m    | 1994-03-17 | NULL       |
+| Bowser   | Diane | dog     | m    | 1989-08-31 | 1995-07-29 |
+| Whistler | Gwen  | bird    | NULL | 1997-12-09 | NULL       |
++----------+-------+---------+------+------------+------------+
+
+
+To find names containing exactly five characters, use five instances of the _ pattern character:
+
+
+mysql> SELECT * FROM pet WHERE name LIKE '_____';
++-------+--------+---------+------+------------+-------+
+| name  | owner  | species | sex  | birth      | death |
++-------+--------+---------+------+------------+-------+
+| Claws | Gwen   | cat     | m    | 1994-03-17 | NULL  |
+| Buffy | Harold | dog     | f    | 1989-05-13 | NULL  |
++-------+--------+---------+------+------------+-------+
+
+
+Regular expression:
+  https://dev.mysql.com/doc/mysql-tutorial-excerpt/8.0/en/pattern-matching.html#:~:text=SQL%20pattern%20matching%20enables%20you,when%20you%20use%20SQL%20patterns.
